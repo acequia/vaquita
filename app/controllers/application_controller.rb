d80be7c2ae62faa1cd2f81669e970f817b6b0da3
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2017-03-28 23:41:54 -0300

    Comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
+  # Including this module does not authenticate by default. A controller
+  # wishing to authenticate a specific action can use, for example,
+  #
+  #   before_action :authenticate_user
+  #
+  # to test for valid credentials for a User model. Knock returns
+  #
+  #   head :unauthorized
+  #
+  # if it can't find them.
   include Knock::Authenticable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# Users own or manage a specific set of devices.
 class User < ApplicationRecord
   has_secure_password
 
