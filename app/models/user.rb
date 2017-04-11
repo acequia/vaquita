8427381e5af803aa3787d87c47547a64b8ea185d
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2017-04-11 12:36:52 -0300

    Add email to jwt claims

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, uniqueness: true
+
+  # Generates these claims for the authentication token
+  def to_token_payload
+    {
+      sub: id,
+      email: email
+    }
+  end
 end
